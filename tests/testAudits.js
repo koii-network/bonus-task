20d56e05ba6c7ffbd8710c046f7c1d95f26ede99
Soma <ldydq970128@gmail.com>
2025-02-11 17:40:06 -0400

    update: apply generate distribution_proposal - generate distribution_proposal function now applied in audit part and work with the new task weight factors - update audit testing function with more dummy data

--- tests/testAudits.js
+++ tests/testAudits.js
@@ -1,5 +1,8 @@
 import { namespaceWrapper } from "@_koii/namespace-wrapper";
-import { taskList } from "../src/modules/globalList.js";
+import { taskList, developerKey } from "../src/modules/globalList.js";
+import { calculateRewards, checkSumTally } from "../src/modules/helpers.js";
+import bs58 from "bs58";
+import { REWARD_PER_ROUND } from "../src/config/constants.js";
 
 // Run the test
 async function testAudit(submission, roundNumber, submitterKey) {
@@ -203,7 +206,198 @@ async function testAudit(submission, roundNumber, submitterKey) {
   }
 
   console.log("Final weighting factors for round:", roundNumber, weighting_factors);
+
+  await generateDistributionProposal(weighting_factors, roundNumber);
 }
+
+async function generateDistributionProposal(weighting_factors, roundNumber) {
+  try {
+    console.log(`generate DistributionProposal FOR ROUND ${roundNumber}`);
+    // you can optionally return this value to be used in debugging
+
+    const getTaskList = taskList;
+    const getWeightList = weighting_factors;
+    const getDeveloperKey = developerKey;
+
+    // the all task states are fetched in parallel
+    const getAllTaskStates = await getTaskState(getTaskList);
+
+    const users = {};
+
+    for (const taskStates of getAllTaskStates) {
+      const { taskId } = taskStates;
+      const { submissions, stake_list, task_manager } = taskStates.data;
+
+      // submission weights and only get the last five submissions
+      const lastFiveKeys = Object.keys(submissions)
+        .map(Number)
+        .sort((a, b) => a - b)
+        .slice(-5);
+
+      // get the sum of all the submission weights
+      for (const lastFiveKey of lastFiveKeys) {
+        const submission = submissions[`${lastFiveKey}`];
+
+        // kpl staking key
+        for (const itemKey of Object.keys(submission)) {
+          if (stake_list.hasOwnProperty(itemKey)) {
+            if (!users[itemKey]) {
+              users[itemKey] = {
+                submissions: {},
+                stakes: {},
+                developerOf: {},
+              };
+            }
+
+            users[itemKey].submissions[taskId] =
+              (users[itemKey].submissions[taskId] || 0) + 1;
+            users[itemKey].stakes[taskId] = stake_list[itemKey] / 1e9;
+          }
+        }
+      }
+
+      const developerKey = await bs58.encode(task_manager);
+      if (getDeveloperKey[developerKey]) {
+        const getTaskId = Object.keys(getDeveloperKey[developerKey])[0];
+        if (getTaskId === taskId) {
+          const kplStakingKey =
+            getDeveloperKey[developerKey][getTaskId].getKPLStakingKey;
+          if (!users[kplStakingKey]) {
+            users[kplStakingKey] = {
+              submissions: {},
+              stakes: {},
+              developerOf: {},
+            };
+            // Initialize submissions and stakes for this task
+            if (stake_list[kplStakingKey]) {
+              users[kplStakingKey].stakes[taskId] =
+                stake_list[kplStakingKey] / 1e9;
+            }
+          }
+          users[kplStakingKey].developerOf[taskId] = true;
+          console.log(
+            `Developer bonus set for ${kplStakingKey} on task ${taskId}`,
+          );
+        }
+      }
+
+      // developer key and task id
+      // const developer_key = await bs58.encode(task_manager);
+      // console.log(developer_key);
+    }
+
+    for (let stakingKey in users) {
+      let user = users[stakingKey];
+      if (user.submissions) {
+        for (let submissionKey in user.submissions) {
+          const getTaskSpecificWeight = getWeightList[submissionKey];
+          if (
+            typeof getTaskSpecificWeight === "number" &&
+            !isNaN(getTaskSpecificWeight)
+          ) {
+            const originalValue = user.submissions[submissionKey];
+            user.submissions[submissionKey] *= getTaskSpecificWeight;
+          } else {
+            console.warn(`Missing or invalid weight for task ${submissionKey}`);
+            user.submissions[submissionKey] *= 1;
+          }
+        }
+      }
+    }
+
+    // console.log('Users object before reward calculation:', JSON.stringify(users, null, 2));
+
+    const distribution_proposal = await calculateRewards(
+      users,
+      REWARD_PER_ROUND,
+    );
+
+    const total_node_bonus = Object.values(distribution_proposal).reduce(
+      (a, b) => a + b,
+      0,
+    );
+    console.log("total_node_bonus:", total_node_bonus / 1e9);
+
+    // get both kpl and koii staking key
+    const getKoiiStakingKey =
+      await namespaceWrapper.getSubmitterAccount("KOII");
+    const getKPLStakingKey = await namespaceWrapper.getSubmitterAccount("KPL");
+
+    const koiiPublicKey = getKoiiStakingKey.publicKey.toBase58();
+    const kplPublicKey = getKPLStakingKey.publicKey.toBase58();
+
+    const getStakingKeys = {
+      getKoiiStakingKey: koiiPublicKey,
+      getKPLStakingKey: kplPublicKey,
+    };
+
+    await namespaceWrapper.storeSet("dist_" + roundNumber, {
+      getStakingKeys,
+      distribution_proposal,
+    });
+
+    let checkSumTotal = await checkSumTally(distribution_proposal);
+    console.log("checkSumTotal", checkSumTotal);
+  } catch (error) {
+    console.error("EXECUTE TASK ERROR:", error);
+  }
+}
+
+async function getTaskState(taskList) {
+    try {
+      const fetchPromises = taskList.map(async (task) => {
+        try {
+          console.log("task", task);
+          const result = await namespaceWrapper.getTaskStateById(
+            task.id,
+            task.type,
+            {
+              is_available_balances_required: true,
+              is_stake_list_required: true,
+              is_submission_required: true,
+            },
+          );
+          // console.log("result", result);
+          if (!result || result.data === null) {
+            console.error(`Task ID ${task.id} returned null data.`);
+            return null;
+          }
+  
+          return {
+            taskId: task.id,
+            success: true,
+            data: result,
+          };
+        } catch (error) {
+          console.error(
+            `Error fetching data for task ID ${task.id}:`,
+            error.message,
+          );
+          return null;
+        }
+      });
+  
+      const timeoutPromise = new Promise((_, reject) =>
+        setTimeout(
+          () => reject(new Error("Timeout: Fetching tasks took too long")),
+          120000,
+        ),
+      );
+  
+      const taskResults = await Promise.race([
+        Promise.all(fetchPromises),
+        timeoutPromise,
+      ]);
+  
+      const successfulTasks = taskResults.filter((result) => result !== null);
+  
+      return successfulTasks;
+    } catch (error) {
+      console.error("Error in fetchAllTaskData:", error);
+      return [];
+    }
+  }
+
 
 // Run the test
 testAudit('', 0, '');
